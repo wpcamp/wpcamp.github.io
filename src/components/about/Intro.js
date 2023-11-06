cba7c4acc0f39aff4b2e5b4aa1db9fc58a5e4e02
Will Campbell <wpcampbell1@gmail.com>
2023-11-06 11:41:36 -0500

    updated bio

--- src/components/about/Intro.js
+++ src/components/about/Intro.js
@@ -15,7 +15,7 @@ const Intro = () => {
           I was born in NYC, grew up in Norwich, Vermont and have also lived in Cambridge, England and Salamanca, Spain.
           In 2023, after graduating from college and finishing 6 months of backpacking across Europe and Asia, I pursued my passion for software engineering by enrolling in a full-time, full stack bootcamp at App Academy.
           During my time there, I honed my technical skills and gained significant experience in JavaScript, React, Redux, Python, Flask, PostgreSQL, Express, Node.js, CSS, HTML, and other technologies.
-          Currently, I'm actively self-learning Solidity.
+          Currently, I am working as a Research Assistant at Dartmouth College's HealthX Lab, on a new project with computer science PhD students and faculty to explore the use of large language models (LLMs) and mobile apps for mental health.
           In my free time I love long-distance hiking, exploring new cuisines, playing squash, bocce, skiing, and listening to podcasts.
           Welcome to my page!
         </p>
